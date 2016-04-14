003eb72f8bb01e0e6565c01849c07e21861d115b
soccercjs2 <soccercjs2@gmail.com>
2016-04-13 22:10:43 -0700

    web scraping is working, now improving

--- TradeMakerScraper/Controllers/LeagueScraperController.cs
+++ TradeMakerScraper/Controllers/LeagueScraperController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -45,6 +46,65 @@ namespace TradeMakerScraper.Controllers
         [EnableCors(origins: "*", headers: "*", methods: "*")]
         public string Post(Projections projections)
         {
+            HttpWebRequest webRequest;
+            StreamReader responseReader;
+            string responseData;
+            CookieContainer cookies = new CookieContainer();
+            StreamWriter requestWriter;
+
+            //string postData = string.Format(null, team.League.UserName, team.League.Password);
+
+            try
+            {
+                //get login  page with cookies
+                webRequest = (HttpWebRequest)WebRequest.Create(team.League.LeagueHost.LoginUrl);
+                webRequest.CookieContainer = cookies;
+
+                //recieve non-authenticated cookie
+                webRequest.GetResponse().Close();
+
+                //post form  data to page
+                webRequest = (HttpWebRequest)WebRequest.Create(team.League.LeagueHost.LoginUrl);
+                webRequest.Method = WebRequestMethods.Http.Post;
+                webRequest.ContentType = "application/x-www-form-urlencoded";
+                webRequest.CookieContainer = cookies;
+                webRequest.ContentLength = postData.Length;
+
+                requestWriter = new StreamWriter(webRequest.GetRequestStream());
+                requestWriter.Write(postData);
+                requestWriter.Close();
+
+                //recieve authenticated cookie
+                webRequest.GetResponse().Close();
+
+                //now we get the authenticated page
+                webRequest = (HttpWebRequest)WebRequest.Create(team.Url);
+                webRequest.CookieContainer = cookies;
+                responseReader = new StreamReader(webRequest.GetResponse().GetResponseStream());
+                responseData = responseReader.ReadToEnd();
+                responseReader.Close();
+
+                //load html into htmlagilitypack
+                HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+                document.LoadHtml(responseData);
+
+                HtmlNode table = document.GetElementbyId(League.LeagueHost.StarterTableName);
+                foreach (Player player in Players)
+                {
+                    if (table.InnerHtml.ToLower().Contains(player.Name.ToLower()) && table.InnerHtml.ToLower().Contains(player.Position.ToLower()) &&
+                            (table.InnerHtml.ToLower().Contains(player.NflTeam.ToLower()) || table.InnerHtml.ToLower().Contains(player.NflAlternateTeam.ToLower())))
+                    {
+                        try
+                        {
+                            player.TeamId = team.TeamId;
+                            //db.SaveChanges();
+                        }
+                        catch (Exception e) { }
+                    }
+                }
+            }
+            catch { }
+
             return "asdf";
         }
     }
---
+++ TradeMakerScraper/Models/League.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TradeMakerScraper.Models
+{
+    public class League
+    {
+    }
+}
