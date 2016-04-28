d06c520126a0ab43417a9215e19df7305283ef22
Collin <Collin@STERANKASURFACE>
2016-04-28 16:54:15 -0700

    starting league settings

--- TradeMakerScraper/Models/League.cs
+++ TradeMakerScraper/Models/League.cs
@@ -10,7 +10,7 @@ namespace TradeMakerScraper.Models
         public string Name { get; set; }
         public string Url { get; set; }
         public bool RequiresLogin { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public int PointsPerPassingTouchdown { get; set; }
+        public decimal PointsPerReception { get; set; }
     }
 }
