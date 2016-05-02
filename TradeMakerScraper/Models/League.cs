a633fd84587e12cbccf954d005d4de047ef3a468
Collin <Collin@STERANKASURFACE>
2016-05-01 17:00:08 -0700

    league roster setup and looking right, need to implement login

--- TradeMakerScraper/Models/League.cs
+++ TradeMakerScraper/Models/League.cs
@@ -10,6 +10,11 @@ namespace TradeMakerScraper.Models
         public string Name { get; set; }
         public string Url { get; set; }
         public bool RequiresLogin { get; set; }
+        public int Quarterbacks { get; set; }
+        public int RunningBacks { get; set; }
+        public int WideReceivers { get; set; }
+        public int TightEnds { get; set; }
+        public int Flexes { get; set; }
         public int PointsPerPassingTouchdown { get; set; }
         public int YardsPerFantasyPoint { get; set; }
         public int PointsLostPerInterception { get; set; }
